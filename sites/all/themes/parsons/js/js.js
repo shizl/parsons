a669153e4d084a1e453250b377df2604e2b51579
Max Shi <zhong.liang@yeaot.net>
2016-03-28 16:58:15 +0800

    updated

--- sites/all/themes/parsons/js/js.js
+++ sites/all/themes/parsons/js/js.js
@@ -23,6 +23,89 @@ jQuery('#block-block-2 .free').click(function(){
 
 });
 
+jQuery('.home-video .video-button .button1').click(function(){
+
+ jQuery('body').overlay({
+	 //preventDefault();
+	  effect: 'fade',
+	  opacity: 0.6,
+	  closeOnClick:true, 
+	 onShow: function() {
+     	jQuery('.home-video .video-content1').show();
+	 },
+	onHide: function() {
+         jQuery('.home-video .video-content1').hide();
+	}, 
+	});
+
+
+});
+
+jQuery('.home-video .video-content1 .close1').click(function(){
+
+jQuery("body .overlay").remove();
+jQuery('.home-video .video-content1').hide();
+ jQuery("body").removeClass("overlay-trigger");
+ jQuery("body").css({"overflow":"inherit"});
+
+});
+
+
+jQuery('.home-video .video-button .button2').click(function(){
+
+ jQuery('body').overlay({
+	 //preventDefault();
+	  effect: 'fade',
+	  opacity: 0.6,
+	  closeOnClick:true, 
+	 onShow: function() {
+     	jQuery('.home-video .video-content2').show();
+	 },
+	onHide: function() {
+         jQuery('.home-video .video-content2').hide();
+	}, 
+	});
+
+
+});
+
+jQuery('.home-video .video-content2 .close2').click(function(){
+
+jQuery("body .overlay").remove();
+jQuery('.home-video .video-content2').hide();
+ jQuery("body").removeClass("overlay-trigger");
+ jQuery("body").css({"overflow":"inherit"});
+
+});
+
+
+jQuery('.home-video .video-button .button3').click(function(){
+
+ jQuery('body').overlay({
+	 //preventDefault();
+	  effect: 'fade',
+	  opacity: 0.6,
+	  closeOnClick:true, 
+	 onShow: function() {
+     	jQuery('.home-video .video-content3').show();
+	 },
+	onHide: function() {
+         jQuery('.home-video .video-content3').hide();
+	}, 
+	});
+
+
+});
+
+jQuery('.home-video .video-content3 .close3').click(function(){
+
+jQuery("body .overlay").remove();
+jQuery('.home-video .video-content3').hide();
+ jQuery("body").removeClass("overlay-trigger");
+ jQuery("body").css({"overflow":"inherit"});
+
+});
+
 
 
 });
