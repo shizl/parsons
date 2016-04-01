48beb9f0fe7303786d6942df43abfcc14a350d51
maka <makawu@yeaoh.net>
2016-04-01 14:06:15 +0800

    28

--- sites/all/themes/parsons/js/subpage.js
+++ sites/all/themes/parsons/js/subpage.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function(){
-
-width = 0;
+ width = 0;
  c_width = 0;
  jQuery('.field-name-field-image .field-items .field-item').each(function(){
   c_width = c_width +460;
@@ -9,9 +8,6 @@ jQuery('.field-name-field-image .field-items ').css({'width':c_width+'px'});
 
    jQuery(".bt-left").click(function(){
    left =  c_width /2;
-
-
-
  if( width<=0 && width > parseInt( '-'+left)){
 width = width-460;
       jQuery(".field-items").animate({left:width+'px'});
@@ -23,4 +19,13 @@ width = width+460;
       jQuery(".field-items").animate({left:width+'px'});
 }
 });
+if(jQuery(document).width()>319&&jQuery(document).width()<961){
+subHeight=jQuery('.field-name-field-image .field-items .field-item').height();
+disHeight=subHeight*3;
+dnum=jQuery(".field-name-field-image .field-items .field-item").length;
+h=dnum*284;
+jQuery('.field-name-field-image .field-items').css('height',h+'px');
+jQuery('.field-name-field-image').css('height',disHeight+'px');
+alert(disHeight);
+}
 });
