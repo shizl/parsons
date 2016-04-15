1bf2f45068b7a00222e5e755a88c0c2ca8e46703
Steve shen <shen.xi@yeaoh.net>
2016-04-15 15:09:07 +0800

    master

--- sites/all/themes/parsons/template.php
+++ sites/all/themes/parsons/template.php
@@ -284,25 +284,10 @@ $page = '<a class=" page-item-0" href="/'.current_path().'?field_new_type_tid='.
 
 }else{
 
-if($allpage <=6){
+  if($allpage <=6){
 for($i=1; $i<=$allpage; $i++){
 
-if(!isset($_GET['page']) && $i== 1){
-
-$page .= '<a class="active page-item-'.($i-1).'" > '.$i.'</a>';
-
-}else if(isset($_GET['page']) && ($i-1)== $_GET['page']){
-$page .= '<a class="active page-item-'.($i-1).'" > '.$i.'</a>';
-}else{
- 
- if($i==1){
-	$page .= '<a class=" page-item-'.($i-1).'" href="/'.current_path().'"> '.$i.'</a>';
- }else{
- 	 $page .= '<a class=" page-item-'.($i-1).'" href="/'.current_path().'?page='.($i-1).'"> '.$i.'</a>';
- }
-
-}
-
+$page .= '<a class="  page-item-'.($i-1).'" href="/'.current_path().'?page='.($i-1).'"> '.$i.'</a>';
 }
 }else{
 
@@ -367,7 +352,7 @@ $output = '<div class="parsons-pager">
 
 jQuery(document).ready(function(){
 
-/*
+
 jQuery(".parsons-pager .parsons-page a").each(function(){
 
 var href = jQuery(this).attr("href");
@@ -396,7 +381,7 @@ jQuery(this).removeClass("active");
 
 
 });
-*/
+
 
 jQuery(".parsons-pager .parsons-tiaozhuan .parsons-submit").click(function(){
 
